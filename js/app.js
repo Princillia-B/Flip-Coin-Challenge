3a648b080d736d46dac0ede39be521bf12edbf13
Princillia <princillia@Princillia.local>
2025-12-30 14:14:11 +0100

    ajout de l'animation et changement de background

--- js/app.js
+++ js/app.js
@@ -9,19 +9,32 @@ const button = document.getElementById("button");
 //Etape 2 : Créer un événement Click
 button.addEventListener("click", () => { //Le handler signifie, si quelqu'un fait l'action entre parenthèse, ici "click", alors => execute la fonction (le handler)
 
-    // Etape 3 : Générer un résultat aléatoire
-    const randomNumber = Math.floor(Math.random() * 2);
+    // Etape bonus : ajout de l'animation
+    // Etape bonus 1 = lancer l'animation avant que la condition ne se produit
+    coinImage.classList.add("spin");
 
-    // Etape 4 : Créer ma Condition    
-    if (randomNumber === 0) {
-        // Etape 4.1 = Changer l'image
-        coinImage.src = "resources/heads.svg";
-        // Etape 4.2 = Change le texte
-        coinText.textContent = "Head";
-    } else {
-        // Etape 4.1 = Changer l'image
-        coinImage.src = "resources/tails.svg";
-        // Etape 4.2 = Change le texte
-        coinText.textContent = "Tail";
-    }
-});
+    // Etape bonus 2 = définir un minuteur pour que la fonction s'exécute plus tard
+    setTimeout( () => { //setTimeout(quoi_faire, quand_le_faire);
+        // Etape 3 : Générer un résultat aléatoire
+        const randomNumber = Math.floor(Math.random() * 2);
+
+        // Etape 4 : Créer ma Condition    
+        if (randomNumber === 0) {
+            // Etape 4.1 = Changer l'image
+            coinImage.src = "resources/heads.svg";
+            // Etape 4.1.1 = déclencher l'animation
+
+            // Etape 4.2 = Change le texte
+            coinText.textContent = "Head";
+        } else {
+            // Etape 4.1 = Changer l'image
+            coinImage.src = "resources/tails.svg";
+            // Etape 4.2 = Change le texte
+            coinText.textContent = "Tail";
+        }
+
+        // Etape bonus 3 = retirer la classe pour relancer l'animation
+        coinImage.classList.remove("spin"); 
+    }, 600);
+
+})
